665b73ecd930d9618593ff08738e740917601f49
Ariel Herman <aeh.herman@gmail.com>
2018-03-09 09:48:26 -0500

    add binary search

--- src/LightsView.js
+++ src/LightsView.js
@@ -1,17 +1,35 @@
-import React, { Component } from 'react';
-import LightItem from './LightItem';
-import Config from './config';
+import React, { Component } from "react";
+import LightItem from "./LightItem";
+import Config from "./config";
+
+// In very simplyfied code:
+//
+// do
+//
+//   Query bridge for status of lights
+//
+//     do while more lights available
+//
+//        if lightsaved.unreachable = true and light.unreachable =false
+//
+//             Setstate light to favorite
+//
+//       end if
+//
+//       lightsaved = light
+//
+//     loop
+//
+//     sleep 2000 ms
+//
+// loop
 
 class LightsView extends Component {
   constructor(props) {
     super(props);
     this.requestFailed = false;
     this.data = null;
-
-    this.onToggleLight = this.onToggleLight.bind(this);
-    this.fetchData = this.fetchData.bind(this);
-    this.onBrightnessChanged = this.onBrightnessChanged.bind(this);
-    setInterval(this.fetchData,5000);
+    setInterval(this.fetchData, 5000);
   }
 
   componentWillMount() {
@@ -19,63 +37,69 @@ class LightsView extends Component {
   }
 
   getUrlWithUsername() {
-    return Config.apiUrl + '/api/' + Config.username + '/lights';
+    return Config.apiUrl + "/api/" + Config.username + "/lights";
   }
 
-  fetchData() {
+  fetchData = () => {
     let url = this.getUrlWithUsername();
 
     fetch(url)
       .then(response => {
         if (!response.ok) {
-            throw Error('Network request failed');
-        }    
-        return response;
-      })
-      .then(d => d.json())
-      .then(d => {
-        this.data = d;
-        this.requestFailed = false;
-        this.setState({newData:new Date()});
-      }, () => {
-        this.requestFailed = true;
-        this.setState({newData:new Date()});
-      })
-  }
-
-  changeState(id, bodyData) {
-    let url = this.getUrlWithUsername() + '/' + id + '/state';
-
-    fetch(url, { method: 'PUT', body: bodyData })
-      .then(response => {
-        if (!response.ok) {
-          throw Error('Network request failed');
+          throw Error("Network request failed");
         }
         return response;
       })
       .then(d => d.json())
-      .then(d => {
-        this.requestFailed = false;
-        this.fetchData();
-      }, () => {
-        this.requestFailed = true;
+      .then(
+        d => {
+          this.data = d;
+          this.requestFailed = false;
+          this.setState({ newData: new Date() });
+        },
+        () => {
+          this.requestFailed = true;
+          this.setState({ newData: new Date() });
+        }
+      );
+  };
+
+  changeState = (id, bodyData) => {
+    let url = this.getUrlWithUsername() + "/" + id + "/state";
+
+    fetch(url, { method: "PUT", body: bodyData })
+      .then(response => {
+        if (!response.ok) {
+          throw Error("Network request failed");
+        }
+        return response;
       })
-  }
+      .then(d => d.json())
+      .then(
+        d => {
+          this.requestFailed = false;
+          this.fetchData();
+        },
+        () => {
+          this.requestFailed = true;
+        }
+      );
+  };
 
-  onToggleLight(id, isOn) {
-    let bodyData = '{"on":' + !isOn + '}';
+  onToggleLight = (id, isOn) => {
+    let bodyData = '{"on":' + !isOn + "}";
     this.changeState(id, bodyData);
-  }
+  };
 
-  onBrightnessChanged(id, newValue) {
-    let bodyData = '{"bri":' + newValue + '}';
+  onBrightnessChanged = (id, newValue) => {
+    let bodyData = '{"bri":' + newValue + "}";
     this.changeState(id, bodyData);
-  }
+  };
 
   render() {
     if (this.requestFailed) {
       let url = this.getUrlWithUsername();
-      return <p className='warning'>Could not fetch from {url}</p>
+      return <p className="warning">Could not fetch from {url}</p>;
     }
 
     if (!this.data) {
@@ -83,25 +107,32 @@ class LightsView extends Component {
     }
 
     if (this.data[0] !== undefined) {
-      return <p className='warning'>{this.data[0].error.description}</p>;
+      return <p className="warning">{this.data[0].error.description}</p>;
     }
 
     let data = this.data;
     let lightItems = [];
     let toggleHandler = this.onToggleLight;
     let brightnessHandler = this.onBrightnessChanged;
-    Object.keys(data).forEach(function(id,index) {
+    Object.keys(data).forEach(function(id, index) {
       let item = data[id];
-      let light = <LightItem key={id} id={id} name={data[id].name} 
-                  isOn={item.state.on} bri={item.state.bri} 
-                  reachable={item.state.reachable} 
-                  onToggleLight={toggleHandler}
-                  onBrightnessChanged={brightnessHandler}/>
+      let light = (
+        <LightItem
+          key={id}
+          id={id}
+          name={data[id].name}
+          isOn={item.state.on}
+          bri={item.state.bri}
+          reachable={item.state.reachable}
+          onToggleLight={toggleHandler}
+          onBrightnessChanged={brightnessHandler}
+        />
+      );
       lightItems.push(light);
     });
 
     return (
-      <div align='center' style={{maxWidth:950,margin: '20px auto 0'}}>
+      <div align="center" style={{ maxWidth: 950, margin: "20px auto 0" }}>
         {lightItems}
       </div>
     );
